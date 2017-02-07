2e69565837b9690cfd4c9922a2968c21426489b2
max_xu <xujialv@gmail.com>
2017-02-06 19:52:20 -0500

    merge

--- js/quiz.js
+++ js/quiz.js
@@ -1,6 +1,16 @@
-(function(){
+(function(){ 
 
-	var app = angular.module('myQuiz',[]);
+  angular.module('myQuiz', []).run(function() {
+      FastClick.attach(document.body);
+});
+
+	var app = angular.module('myQuiz',['smoothScroll']);
+
+  app.filter('html', ['$sce', function ($sce) { 
+    return function (text) {
+        return $sce.trustAsHtml(text);
+    };    
+}])
 
 	app.controller('QuizController', ['$scope','$http','$sce',function($scope,$http,$sce){
     
@@ -8,6 +18,7 @@
         $scope.activeQuestion = -1;
         $scope.activeQuestionAnswered = 0;
         $scope.percentage = 0;
+        $scope.result = '测试结果';
 
         $http.get('quiz_data.json').then(function(quizData){
           $scope.myQuestions = quizData.data;
@@ -30,7 +41,19 @@
               }        
               $scope.myQuestions[qIndex].questionState = 'answered'
           } 
-          $scope.percentage = (($scope.score / $scope.totalQuestions)*100).toFixed(1);
+          $scope.percentage = (($scope.score / $scope.totalQuestions)*100).toFixed(0);
+          if($scope.score > 6) {
+            $scope.result = '信用卡经理人';
+          } else if($scope.score <= 6 && $scope.score > 4) { 
+            $scope.result = '信用卡高级玩家';
+          } else if($scope.score <= 4 && $scope.score > 2) { 
+            $scope.result = '信用卡普通玩家';
+          } else if($scope.score <= 4 && $scope.score > 2) { 
+            $scope.result = '信用卡低端玩家';
+          } else if($scope.score <= 4 && $scope.score > 2) { 
+            $scope.result = '信用卡小白';
+          }
+          document.title = '我对信用卡的了解程度有' + $scope.percentage + '%，达到' + $scope.result + '水平';
         }
 
        $scope.isSelected = function(qIndex,aIndex){
@@ -40,7 +63,7 @@
           return $scope.myQuestions[qIndex].correctAnswer === aIndex;
        }
 
-       $scope.selectContinue = function(){
+       $scope.selectContinue = function(){ 
           return $scope.activeQuestion += 1;
        }
 
@@ -48,14 +71,39 @@
           
           var url = 'http://aquiz.cn/quiz2'
 
-          var emailLink = '<a class="btn email" href="#">Email a friend</a>'
+          var mqLink = ''
 
-          var wechatLink = '<a class="btn wechat" target="_blank" href="#">联系开发者</a>';    
+          var wechatLink = '';    
            
-          var newMarkup = emailLink + wechatLink;
+          var newMarkup = wechatLink + mqLink;
 
-          return $sce.trustAsHtml(newMarkup);
-        
+          return $sce.trustAsHtml(newMarkup); 
+       }
+
+       $scope.resizeHeight = function(){
+          document.getElementById('myQuiz').style.height = 1150 + 'px'; 
+       }
+       
+       $scope.addOverlay = function(){  
+        var myOverlay = document.createElement('div');
+        myOverlay.id = 'overlay'; 
+        document.body.appendChild(myOverlay); 
+        myOverlay.style.position = 'absolute';
+        myOverlay.style.top = 0;  
+        myOverlay.style.opacity = 0.8;  
+        myOverlay.style.width = window.innerWidth + 'px';
+        myOverlay.style.height = document.body.clientHeight + 'px';
+        myOverlay.style.top = window.pageYOffset + 'px';
+        myOverlay.style.left = window.pageXOffset + 'px';
+        myOverlay.style.zIndex = 999;
+        myOverlay.style.backgroundColor = '#000';  
+        document.getElementById('developer').style.visibility = 'visible';
+        myOverlay.addEventListener('click', removeSelf, false);
+       }
+       
+       function removeSelf() {
+           document.getElementById('overlay').remove();
+           document.getElementById('developer').style.visibility = 'hidden';
        }
 
 	}]);
